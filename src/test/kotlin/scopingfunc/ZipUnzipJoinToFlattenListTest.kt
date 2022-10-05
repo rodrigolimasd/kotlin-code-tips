5f1a07c9e597372af4981742defd6bca78c0bd6f
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-10-05 20:45:32 -0300

    adjusts

--- src/test/kotlin/scopingfunc/ZipUnzipJoinToFlattenListTest.kt
+++ src/test/kotlin/scopingfunc/ZipUnzipJoinToFlattenListTest.kt
@@ -3,7 +3,7 @@ package scopingfunc
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
-class ZipUnzipJoinToListTest {
+class ZipUnzipJoinToFlattenListTest {
 
     @Test
     fun `should create list of tuples with two list`(){
@@ -27,6 +27,12 @@ class ZipUnzipJoinToListTest {
         val listString = StringBuffer("The list of numbers: ")
         val joined = numbers.joinTo(listString)
         Assertions.assertEquals("The list of numbers: one, two, three, four", joined.toString())
+    }
+
+    @Test
+    fun `should creates one list from the list of lists`(){
+        val listOfList = listOf(listOf(1,2), listOf(3,4,5), listOf(6,7))
+        Assertions.assertIterableEquals(listOf(1,2,3,4,5,6,7), listOfList.flatten())
     }
 
 }
