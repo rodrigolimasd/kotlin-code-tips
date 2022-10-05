d64d55c552a89b8fc63096e90a7685f4acb21fc1
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-10-05 20:38:27 -0300

    kotlin list join in string

--- src/test/kotlin/scopingfunc/MapTest.kt
+++ src/test/kotlin/scopingfunc/MapTest.kt
@@ -2,7 +2,6 @@ package scopingfunc
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import kotlin.math.exp
 
 class MapTest {
 
@@ -24,22 +23,5 @@ class MapTest {
             listOf(2,6,12,20),
             numbers.mapIndexedNotNull{i, v -> if (i == 0) null else v * i}
         )
-    }
-
-    @Test
-    fun `should create list of tuples with two list`(){
-        val colors = listOf("red", "brown", "grey")
-        val animals = listOf("fox", "bear", "wolf")
-        val expected = listOf("red" to "fox", "brown" to "bear", "grey" to "wolf")
-        Assertions.assertIterableEquals(expected, colors.zip(animals))
-    }
-
-    @Test
-    fun `should create two list from one list of pair`(){
-        val numberPairs = listOf("one" to 1, "two" to 2, "three" to 3, "four" to 4)
-        val unzipedPair = numberPairs.unzip()
-        Assertions.assertIterableEquals(listOf("one","two","three","four"), unzipedPair.first)
-        Assertions.assertIterableEquals(listOf(1,2,3,4), unzipedPair.second)
-
     }
 }
---
+++ src/test/kotlin/scopingfunc/ZipUnzipJoinToListTest.kt
@@ -0,0 +1,32 @@
+package scopingfunc
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ZipUnzipJoinToListTest {
+    
+    @Test
+    fun `should create list of tuples with two list`(){
+        val colors = listOf("red", "brown", "grey")
+        val animals = listOf("fox", "bear", "wolf")
+        val expected = listOf("red" to "fox", "brown" to "bear", "grey" to "wolf")
+        Assertions.assertIterableEquals(expected, colors.zip(animals))
+    }
+
+    @Test
+    fun `should create two list from one list of pair`(){
+        val numberPairs = listOf("one" to 1, "two" to 2, "three" to 3, "four" to 4)
+        val unzipedPair = numberPairs.unzip()
+        Assertions.assertIterableEquals(listOf("one","two","three","four"), unzipedPair.first)
+        Assertions.assertIterableEquals(listOf(1,2,3,4), unzipedPair.second)
+    }
+
+    @Test
+    fun `should join StringBuffer in list`(){
+        val numbers = listOf("one", "two", "three", "four")
+        val listString = StringBuffer("The list of numbers: ")
+        val joined = numbers.joinTo(listString)
+        Assertions.assertEquals("The list of numbers: one, two, three, four", joined.toString())
+    }
+
+}
