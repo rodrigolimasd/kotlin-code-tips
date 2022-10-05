f39e5a9ac7ba20a3a8422c84e7dd36cb1651a9fd
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-10-05 20:50:35 -0300

    kotlin using any, none and all funs list

---
+++ src/test/kotlin/scopingfunc/AnyNoneAllListTest.kt
@@ -0,0 +1,16 @@
+package scopingfunc
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class AnyNoneAllListTest {
+
+    @Test
+    fun `should check any none all from list`(){
+        val numbers = listOf("one", "two", "three", "four")
+
+        Assertions.assertTrue(numbers.any { it.endsWith("e") } )
+        Assertions.assertTrue(numbers.none { it.endsWith("a") })
+        Assertions.assertFalse(numbers.all { it.endsWith("e") })
+    }
+}
