517847de7b82d1422f1d486b86851e9f3f6ab7a3
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-10-05 20:43:46 -0300

    kotlin using flatten fun collections

--- src/test/kotlin/scopingfunc/FlattenTest.kt
+++ src/test/kotlin/scopingfunc/FlattenTest.kt
@@ -1,4 +1,13 @@
 package scopingfunc
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 class FlattenTest {
+
+    @Test
+    fun `should creates one list from the list of lists`(){
+        val listOfList = listOf(listOf(1,2), listOf(3,4,5), listOf(6,7))
+        Assertions.assertIterableEquals(listOf(1,2,3,4,5,6,7), listOfList.flatten())
+    }
 }
