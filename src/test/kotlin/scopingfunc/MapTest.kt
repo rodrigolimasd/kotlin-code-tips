c6c205cb1765c531e2cf3085138fcb7c34b6fe7f
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-10-05 20:11:02 -0300

    kotlin using map collections funcs

---
+++ src/test/kotlin/scopingfunc/MapTest.kt
@@ -0,0 +1,29 @@
+package scopingfunc
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class MapTest {
+
+    @Test
+    fun `should using map transform object`() {
+        val numbers = listOf(1,2,3,4,5)
+        numbers.map { it * 3 }
+
+        Assertions.assertIterableEquals(listOf(3,6,9,12,15), numbers.map { it * 3 })
+
+        Assertions.assertIterableEquals(listOf(0,2,6,12,20),
+            numbers.mapIndexed{ index, value -> value * index})
+
+        Assertions.assertIterableEquals(
+            listOf(3,9,12,15),
+            numbers.mapNotNull { v-> if(v==2) null else v * 3 })
+
+        Assertions.assertIterableEquals(
+            listOf(2,6,12,20),
+            numbers.mapIndexedNotNull{i, v -> if (i == 0) null else v * i}
+        )
+
+
+    }
+}
